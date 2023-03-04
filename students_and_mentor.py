949fd91214333850356de469c4b5f16afb6d9ff1
e12io898 <e12io898@ya.ru>
2023-03-05 01:31:50 +0300

    task_2

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -8,7 +8,9 @@ class Student:
         self.grades = {}
 
     def rate_lec(self, lector, course, grade):
-        if isinstance(lector, Lecturer) and course in self.courses_in_progress and course in lector.courses_attached:
+        if isinstance(lector, Lecturer) \
+                and course in self.courses_in_progress \
+                and course in lector.courses_attached:
             if course in lector.grades:
                 lector.grades[course] += [grade]
             else:
@@ -21,14 +23,17 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        self.grades = {}
 
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if isinstance(student, Student) \
+                and course in self.courses_attached \
+                and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
